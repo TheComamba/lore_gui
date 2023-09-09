d32147d9a3b0270db4c2ed5a1dea02073da56456
TheComamba <thecomamba@jojoheinze.de>
2023-09-09 17:07:38 +0200

    Name field for new entity dialogue.

--- src/dialog/new_entity.rs
+++ src/dialog/new_entity.rs
@@ -9,14 +9,16 @@ use iced::{
 #[derive(Debug, Clone)]
 pub(crate) struct NewEntityDialog {
     label: String,
-    ent_type: String,
+    ent_category: String,
+    name: String,
 }
 
 impl NewEntityDialog {
     pub(crate) fn new() -> Self {
         NewEntityDialog {
             label: String::new(),
-            ent_type: String::new(),
+            ent_category: String::new(),
+            name: String::new(),
         }
     }
 }
@@ -42,8 +44,12 @@ impl Component<GuiMes, Renderer> for NewEntityDialog {
                 self.label = label;
                 None
             }
-            NewEntityMes::TypeUpd(ent_type) => {
-                self.ent_type = ent_type;
+            NewEntityMes::CategoryUpd(ent_type) => {
+                self.ent_category = ent_type;
+                None
+            }
+            NewEntityMes::NameUpd(name) => {
+                self.name = name;
                 None
             }
             NewEntityMes::Submit => Some(GuiMes::EntitySubmit),
@@ -52,13 +58,16 @@ impl Component<GuiMes, Renderer> for NewEntityDialog {
 
     fn view(&self, _state: &Self::State) -> Element<'_, Self::Event, Renderer> {
         let label_input = TextInput::new("", &self.label).on_input(NewEntityMes::LabelUpd);
-        let type_input = TextInput::new("", &self.ent_type).on_input(NewEntityMes::TypeUpd);
+        let type_input = TextInput::new("", &self.ent_category).on_input(NewEntityMes::CategoryUpd);
+        let name_input = TextInput::new("", &self.name).on_input(NewEntityMes::NameUpd);
         let submit_button = Button::new(Text::new("Create")).on_press(NewEntityMes::Submit);
         Column::new()
             .push(Text::new("Label:"))
             .push(label_input)
-            .push(Text::new("Type:"))
+            .push(Text::new("Category:"))
             .push(type_input)
+            .push(Text::new("Name"))
+            .push(name_input)
             .push(submit_button)
             .padding(5)
             .spacing(5)
@@ -69,6 +78,7 @@ impl Component<GuiMes, Renderer> for NewEntityDialog {
 #[derive(Debug, Clone)]
 pub(crate) enum NewEntityMes {
     LabelUpd(String),
-    TypeUpd(String),
+    CategoryUpd(String),
+    NameUpd(String),
     Submit,
 }
